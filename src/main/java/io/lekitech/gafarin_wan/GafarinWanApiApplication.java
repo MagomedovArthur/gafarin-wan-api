ac6fffaaa99ec23e49558d7bc38638a6a4d2c946
Arthur Magomedov <devmagomedov@gmail.com>
2024-10-28 22:10:56 +0300

    init

---
+++ src/main/java/io/lekitech/gafarin_wan/GafarinWanApiApplication.java
@@ -0,0 +1,13 @@
+package io.lekitech.gafarin_wan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GafarinWanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GafarinWanApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/lekitech/gafarin_wan/GafarinWanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lekitech.gafarin_wan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GafarinWanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
